a340d0424350895ff8564d439ba29ca5929430db
Ida Rosvall <ir222gn@student.lnu.se>
2021-07-07 14:44:44 +0200

    Init project

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace auth_service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountController : ControllerBase
+    {
+        public AccountController()
+        {
+        }
+    }
+}
