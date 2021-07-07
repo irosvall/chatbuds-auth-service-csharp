5936e605e0cd6bb8346582799282ba86b6a0bb4e
Ida Rosvall <ir222gn@student.lnu.se>
2021-07-07 16:18:13 +0200

    Setup simple account class

---
+++ Models/Account/Account.cs
@@ -0,0 +1,13 @@
+﻿namespace auth_service.Models.Account
+{
+    public class Account
+    {
+        public string Id { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Username { get; set; }
+        
+        public string Password { get; set; }
+    }
+}
