4b612edb2728a1e9b0e1f1b29f710ccce769ec5f
Ida Rosvall <ir222gn@student.lnu.se>
2021-08-21 16:05:36 +0200

    Make the account model work with MongoDB

--- Models/Account.cs
+++ Models/Account.cs
@@ -1,7 +1,12 @@
-﻿namespace auth_service.Models.Account
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace auth_service.Models
 {
     public class Account
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         
         public string Email { get; set; }
