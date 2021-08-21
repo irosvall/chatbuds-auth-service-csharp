49ba1ae892bb67a42fe717709c949bd198440543
Ida Rosvall <ir222gn@student.lnu.se>
2021-08-21 16:23:50 +0200

    Create an account service interface

---
+++ Services/AccountService/IAccountService.cs
@@ -0,0 +1,22 @@
+﻿using auth_service.Models;
+
+namespace auth_service.Services.AccountService
+{
+    public interface IAccountService
+    {
+        /// <summary>
+        /// Authenticates the account.
+        /// </summary>
+        Account AuthenticateAccount(string email, string password);
+        
+        /// <summary>
+        /// Creates a new account.
+        /// </summary>
+        Account CreateAccount(Account account);
+
+        /// <summary>
+        /// Deletes the account.
+        /// </summary>
+        void DeleteAccount(Account account);
+    }
+}
