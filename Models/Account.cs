c6e181d9494426dd7f0085bd2540c88c5cca6f3e
Ida Rosvall <ir222gn@student.lnu.se>
2021-08-22 16:40:10 +0200

    Update the account model to make the fields match the database

--- Models/Account.cs
+++ Models/Account.cs
@@ -9,10 +9,13 @@ namespace auth_service.Models
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         
+        [BsonElement("email")]
         public string Email { get; set; }
         
+        [BsonElement("username")]
         public string Username { get; set; }
         
+        [BsonElement("password")]
         public string Password { get; set; }
     }
 }
